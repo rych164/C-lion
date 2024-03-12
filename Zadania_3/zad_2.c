b4c1f64b34ca2ab52629b6c12e04b1f99a1cf404
rych164 <107195029+rych164@users.noreply.github.com>
2024-03-12 08:35:07 +0100

    Create zad_2.c

---
+++ Zadania_3/zad_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int generateRandomInt(int lower, int upper)
+{
+    int num=(rand()%(upper-lower+1))+lower;
+    return num;
+}
+int main()
+{
+    srand(time(0));
+    int width;
+    int length;
+    int *ptrWidth;
+    int *ptrLength;
+    printf("width =");
+    scanf("%d",&width);
+    printf("length =");
+    scanf("%d",&length);
+    int* ptr = malloc((length * width) * sizeof(int));
+
+    /* Putting 1 to 12 in the 1D array in a sequence */
+    for (int i = 0; i < length * width; i++)
+        ptr[i] = generateRandomInt(0,100);
+
+    /* Accessing the array values as if it was a 2D array */
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++)
+            printf("%d ", ptr[i * width + j]);
+        printf("\n");
+    }
+
+    free(ptr);
+}
